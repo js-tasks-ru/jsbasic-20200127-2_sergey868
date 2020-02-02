e1b119fc86de7741d76da1923ca41481839a9716
marazmatical <sera45@mail.ru>
2020-02-02 18:59:42 +0300

    Решение задачи isValid

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+
+  return (name  && name.length >= 4 && !name.includes(' ')) || false;
+
+
 }
 
 function sayHello() {
