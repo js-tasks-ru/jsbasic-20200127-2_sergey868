7a436974f9a252c79612a140846482c1c96ecbae
marazmatical <sera45@mail.ru>
2020-02-24 14:49:29 +0300

    Задача из массива в  список.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let list = document.createElement("ul");
+  let friends2 = friends.map(element=>
+
+    `<li>${element.firstName}  ${element.lastName}</li>`).join('');
+
+
+  list.innerHTML = friends2;
+
+  return list;
+
 }
