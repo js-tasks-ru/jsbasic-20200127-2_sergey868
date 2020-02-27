3450fd2fc85c596a469d24b615d60caf756ad00d
marazmatical <sera45@mail.ru>
2020-02-27 19:28:19 +0300

    Решение задач 5.2 и 6.1

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,29 +1,60 @@
-/**
- * Компонент, который реализует сортируемую таблицу
- * @param {Array} items - данные, которые нужно отобразить
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },
- *
- * @constructor
- */
-function SortableTable(items) {
-  /**
-   * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
-   */
-  this.el = document.createElement('table');
 
-  /**
-   * Метод выполняет сортировку таблицы
-   * @param {number} column - номер колонки, по которой
-   * нужно выполнить сортировку (отсчет начинается от 0)
-   * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-   */
-  this.sort = (column, desc = false) => {};
+function SortableTable(items) {
+
+  this.el = document.createElement('table');
+  this.thead = [];
+  this.thead.push(`<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>`);
+  this.createTbody = (items2)=> {
+   items2 = items.map(element=>{
+
+  return `<tr><td>${element.name}</td><td>${element.age}</td><td>${element.salary}</td><td>${element.city}</td></tr>`;
+
+    });
+    items2.unshift("<tbody>");
+    items2.push("</tbody");
+
+    return items2;
+  };
+  this.createTable = ()=>{
+    this.table = this.thead.concat(this.createTbody(items)).join('');
+    this.el.innerHTML = this.table;
+
+  };
+
+  this.createTable();
+
+  this.sort = (column, desc = false) => {
+    if (column === 2 && desc === true) {
+      items.sort((element1, element2) => element2.salary - element1.salary);
+      this.createTable();
+
+    }
+    if (column === 2 && desc === false) {
+      items.sort((element1, element2) => element1.salary - element2.salary);
+      //items.reverse();
+      this.createTable();
+
+    }
+    if (column === 0 && desc === false) {
+      items.sort((a, b)=>{
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        if (a.name === b.name) return 0;
+
+      });
+      this.createTable();
+    }
+    if (column === 0 && desc === true) {
+      items.sort((a, b)=>{
+        if (a.name < b.name) return 1;
+        if (a.name > b.name) return -1;
+        if (a.name === b.name) return 0;
+
+      });
+      //items.reverse();
+      this.createTable();
+    }
+
+  };
+
 }
