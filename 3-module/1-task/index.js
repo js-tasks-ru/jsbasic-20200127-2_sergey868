c8b871d8b1f7292207c28517446b072a8f970595
marazmatical <sera45@mail.ru>
2020-03-11 17:24:31 +0300

    Решение задачи 3.1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,5 @@
+
+
 /**
  * showSalary
  * @param {Array} data - данные о пользователях
@@ -5,4 +7,17 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+
+  console.log(data);
+  let str = '';
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].age <= age) {
+      str += `${data[i].name}, ${data[i].balance}\n`;
+    }
+
+  }
+  str = str.slice(0, str.length - 1);
+
+  return str;
 }
+
