65303d2740144d32dae44b2ebf0386f9dda2f929
marazmatical <sera45@mail.ru>
2020-03-11 19:34:57 +0300

    Решение задач 3.2 и 3.3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let arr = str.split(',');
+  let arr2 = str.split(' ');
+  let arr3 = arr.concat(arr2);
+  let arr1 = [];
+  for (let elem of arr3) {
+    let number = +elem;
+    if (number && typeof number === 'number') arr1.push(number);
+  }
+  arr1.sort((a, b)=>a - b);
+  return {min: arr1[0], max: arr1[arr1.length - 1] };
 }
